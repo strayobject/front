ebdb426c701c4ba5cdce8434836885783867de44
Michal Zdrojewski <code@strayobject.co.uk>
2016-10-23 22:02:58 +0100

    fix(entrypoint): working entrypoint

--- opt/app/entrypoint.sh
+++ opt/app/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# normally we would not want this to be here
+apt-get update && apt-get install cron git zip wget -y
+
+
 bin/composer.phar install
 
 # download letsencrypt client
-wget -P cert/ https://github.com/kelunik/acme-client/releases/download/v0.2.10/acme-client.phar
-chmod +x acme-client.phar
-acme-client.phar auto
+if [ ! -f cert/acme-client.phar ];
+then
+    wget -P cert/ https://github.com/kelunik/acme-client/releases/download/v0.2.10/acme-client.phar
+    chmod +x cert/acme-client.phar
+fi
 
-crontab -l; echo "0 0 * * * cert/acme-client.phar auto; RC=$?; if [ $RC = 4 ] || [ $RC = 5 ]; then vendor/amphp/aerys --restart -c server.php; fi" ) | crontab -
+cert/acme-client.phar auto
+
+(crontab -l; echo "0 0 * * * /var/www/html/cert/acme-client.phar auto; RC=$?; if [ $RC = 4 ] || [ $RC = 5 ]; then /var/www/html/vendor/amphp/aerys --restart -c server.php; fi" ) | crontab -
 
 php -d zend.assertions=-1 vendor/amphp/aerys/bin/aerys -c server.php
