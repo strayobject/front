5c7c5f177161898364a4a6da6d7f129a070c91fc
Michal Zdrojewski <code@strayobject.co.uk>
2016-11-03 14:25:03 +0000

    chore(docker): dev updates

--- opt/app/entrypoint.sh
+++ opt/app/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# normally we would not want this to be here
-apt-get update && apt-get install netcat cron git zip wget -y
+#TODO: move certificate fetching to it's own container
+if [ $DEV ]; then
+    rm -r var/cache/*
+    php -d zend.assertions=1 vendor/amphp/aerys/bin/aerys -d -c server.php
+else
+    # normally we would not want this to be here
+    apt-get update && apt-get install netcat cron git zip wget -y
 
+    bin/composer.phar install
 
-bin/composer.phar install
+    if [ ! -d /root/.acme.sh ];
+    then
+        wget -O -  https://get.acme.sh | sh
+    fi
+    # todo renewal will fail because port is used
+    /root/.acme.sh/acme.sh --issue --standalone --httpport 8080 -d strayobject.co.uk \
+    --fullchainpath "/var/www/html/cert" --keypath "/var/www/html/cert" \
+    --renew-hook "/var/www/html/vendor/amphp/aerys/bin/aerys --restart -c /var/www/html/server.php"
 
-
-if [ ! -d /root/.acme.sh ];
-then
-    wget -O -  https://get.acme.sh | sh
+    php -d zend.assertions=-1 vendor/amphp/aerys/bin/aerys -c server.php
 fi
-
-/root/.acme.sh/acme.sh --issue --standalone --httpport 8080 -d strayobject.co.uk -d www.strayobject.co.uk --renew-hook "/var/www/html/vendor/amphp/aerys/bin/aerys --restart -c /var/www/html/server.php"
-
-php -d zend.assertions=-1 vendor/amphp/aerys/bin/aerys -c server.php
